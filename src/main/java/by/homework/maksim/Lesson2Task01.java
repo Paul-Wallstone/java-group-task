88d94d53f5fca8a0dfad564dd19dbad2ba18fe8d
ppuchinsky <pavel.puchinsky@storemind.de>
2024-05-24 12:13:04 +0300

    init

---
+++ src/main/java/by/homework/maksim/Lesson2Task01.java
@@ -0,0 +1,10 @@
+package by.homework.maksim;
+
+/**
+ * @author ppuchinsky
+ */
+public class Lesson2Task01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/by/homework/sergey/Lesson2Task01.java
@@ -0,0 +1,10 @@
+package by.homework.sergey;
+
+/**
+ * @author ppuchinsky
+ */
+public class Lesson2Task01 {
+    public static void main(String[] args) {
+
+    }
+}
