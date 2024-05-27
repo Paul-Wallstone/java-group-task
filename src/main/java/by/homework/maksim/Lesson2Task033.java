5819596970b94acbfa65b9f6583d697e758af87b
bolemir1927 <maksim_petrov_88@mail.ru>
2024-05-27 19:47:12 +0300

    add 2+ Task

--- src/main/java/by/homework/maksim/Lesson2Task033.java
+++ src/main/java/by/homework/maksim/Lesson2Task033.java
@@ -5,26 +5,29 @@ import java.util.Scanner;
 public class Lesson2Task033 {
     public static void main(String[] args) {
         int randomNum = (int) (Math.random() * 100);
-
+        int count = 1;
+        final int limit = 10;
+        boolean statusWin = false;
         System.out.println("Добро пожаловать в игру \"Угадай число\"!");
         System.out.println("Я загадал число от 1 до 100. Попробуй угадать!");
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Попытка 1. Введите вашу догадку: ");
-        int numberOne = sc.nextInt();
-        while (true) {
-            int number = 0;
-            for (int i = 0; i < 10; i++) ;
-            int numberTwo = 9;
-            for (int i = 0; i > 0; --i) ;
+        while (count <= limit) {
+            System.out.println("Попытка 1. Введите вашу догадку: ");
+            int numberOne = sc.nextInt();
+            count++;
             if (numberOne == randomNum) {
-                System.out.println("Поздравляю! Вы угадали число " + randomNum + " с " + number + " попытки!");
+                System.out.println("Поздравляю! Вы угадали число " + randomNum + " с " + count + " попытки!");
+                statusWin = true;
                 break;
             } else if (numberOne > randomNum) {
-                System.out.println("Меньше. У вас осталось " + numberTwo + " попыток.");
+                System.out.println("Меньше. У вас осталось " + (limit - count) + " попыток.");
             } else if (numberOne < randomNum) {
-                System.out.println("Больше. У вас осталось " + numberTwo + " попыток.");
+                System.out.println("Больше. У вас осталось " + (limit - count) + " попыток.");
             }
+        }
+        if (!statusWin) {
+            System.out.println("проиграл");
         }
         sc.close();
     }
