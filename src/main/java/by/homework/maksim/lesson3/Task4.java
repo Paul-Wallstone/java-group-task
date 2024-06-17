514c4d4fc8a1186b805eb5801bb85974061c92f1
bolemir1927 <maksim_petrov_88@mail.ru>
2024-06-17 20:09:47 +0300

    lesson3 add tasks

--- src/main/java/by/homework/maksim/lesson3/Task4.java
+++ src/main/java/by/homework/maksim/lesson3/Task4.java
@@ -1,22 +1,18 @@
 package by.homework.maksim.lesson3;
 
-
 public class Task4 {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 4, 3, 5};
+        int[] numbers = {1, 2, 4, 32, 3};
         int target = 3;
         System.out.println("Индекс числа " + target + ": " + findIndex(numbers, target));
     }
 
-    public static int findIndex(int[] nums, int target) {
-        int index = -1;
-        for (int i = 0, i <nums.length, i++){
-            if (nums[i] == target) {
-                index = i;
-                return nums[i];
+    public static int findIndex(int[] numbers, int target) {
+        for (int i = 0; i <numbers.length; i++){
+            if (numbers[i] == target) {
+                return i;
             }
-            return -1;
         }
-
+        return -1;
     }
 }
