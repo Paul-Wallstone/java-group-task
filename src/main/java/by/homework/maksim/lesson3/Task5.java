35803e3717858f1a444515b4588550fdc6c00e83
bolemir1927 <maksim_petrov_88@mail.ru>
2024-06-19 21:18:58 +0300

    lesson3 add tasks

--- src/main/java/by/homework/maksim/lesson3/Task5.java
+++ src/main/java/by/homework/maksim/lesson3/Task5.java
@@ -7,14 +7,19 @@ public class Task5 {
                 {4, 5, 6},
                 {7, 8, 9}
         };
-     //   int[] sums = sumRowsAndColumns(matrix);
-   //     System.out.println("Сумма всех строк: " + sums[0]);
-   //     System.out.println("Сумма всех столбцов: " + sums[1]);
+        int[] sums = sumRows(matrix);
+        System.out.println("Сумма всех строк: " + sums[0]);
+    }
 
-    //    public static int sumRowsAndColumns ( int[] matrix){
-    //        int sumRowsAndColumns = 0;
-    //        for (int i = 0; i < matrix.length; i++) {
-   //             for (int j = 0; j < matrix[i].length; j++)
-//int[] sums += matrix[i];
+    public static int[] sumRows(int[][] numbers) {
+        int[] sums = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < numbers[i].length; j++) {
+                sum += numbers[i][j];
             }
+            sums[i] = sum;
         }
+        return sums;
+    }
+}
