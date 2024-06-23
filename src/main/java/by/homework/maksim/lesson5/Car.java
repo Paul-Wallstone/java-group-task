01e5cfa355b4411cc7a7005ffd71c9f8ea83fc25
bolemir1927 <maksim_petrov_88@mail.ru>
2024-06-23 17:08:52 +0300

    lesson3 add tasks

---
+++ src/main/java/by/homework/maksim/lesson5/Car.java
@@ -0,0 +1,11 @@
+package by.homework.maksim.lesson5;
+
+public class Car {
+private String name;
+private double power;
+private double maxPilotWeigh;
+Pilot pilot;
+
+
+
+}
---
+++ src/main/java/by/homework/maksim/lesson5/RaceTrack.java
@@ -0,0 +1,4 @@
+package by.homework.maksim.lesson5;
+
+public class RaceTrack {
+}
