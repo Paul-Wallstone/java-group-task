8ffdd088a348ee6d45902383fab8cc78eb3b5c10
ppuchinsky <pavel.puchinsky@storemind.de>
2024-08-28 14:50:15 +0300

    lesson 12 refactor

--- src/main/java/by/homework/maksim/lesson12/task2/Main.java
+++ src/main/java/by/homework/maksim/lesson12/task2/Main.java
@@ -1,6 +1,11 @@
 package by.homework.maksim.lesson12.task2;
 
 public class Main {
+    public static void main(String[] args) {
+        boolean result = PalindromeChecker.isPalindrome("hello");
+        System.out.println(result);
+    }
+
 
     //"world", "hello", "madam", "level"
 }
