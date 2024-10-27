219f3756111987cc857715fcaabc6833a3ff4984
ppuchinsky <pavel.puchinsky@storemind.de>
2024-10-27 13:05:41 +0300

    lesson 21 - lesson

---
+++ src/main/java/by/homework/lessons/lesson21/App.java
@@ -0,0 +1,33 @@
+package by.homework.lessons.lesson21;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+/**
+ * @author ppuchinsky
+ */
+public class App {
+
+    public static final String EXIT = "4";
+
+    public static void main(String[] args) {
+        boolean exit = false;
+        String name = "";
+        while (!exit) {
+            System.out.println("Enter name:");
+            Scanner scanner = new Scanner(System.in);
+            name = scanner.next();
+            System.out.println("Enter text:");
+            String next = scanner.next();
+            if (EXIT.equals(next)) {
+                exit = true;
+            }
+            System.out.println("%s - %s: "
+                    .formatted(name, LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-YYYY hh:mm")))
+                    + next);
+        }
+
+        System.out.println("Exit");
+    }
+}
